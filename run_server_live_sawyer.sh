9a71005fcfb8c17090960b1237bd51fca8174451
tedyptedto <nomp5923@gmail.com>
2022-08-01 22:49:52 +0200

    update sawyer

--- run_server_live_sawyer.sh
+++ run_server_live_sawyer.sh
@@ -8,6 +8,6 @@ do
     :
     echo "Running screen on $i"
     screen -S "sawyer_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_sawyer $i  configs/live/a_sawyer.json "
-    sleep 20
+    sleep 1
 done
 
