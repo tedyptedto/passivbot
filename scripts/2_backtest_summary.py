d47f265060f45fc8e5766c9c7dbb95cd5a7fb9da
tedyptedto <nomp5923@gmail.com>
2022-08-27 12:23:24 +0200

    backtest summary : only long data backtest summary : news filters

--- scripts/2_backtest_summary.py
+++ scripts/2_backtest_summary.py
@@ -77,6 +77,21 @@ def arguments_management():
                         help="Minimum grid span",
     )
 
+    parser.add_argument("-min-eqbal_ratio_min_long","--min-eqbal_ratio_min_long",
+                        type=float,required=False,dest="min_eqbal_ratio_min_long",default=0,
+                        help="Minimum eqbal_ratio_min_long",
+    )
+
+    parser.add_argument("-max-pa_distance_mean_long","--max-pa_distance_mean_long",
+                        type=float,required=False,dest="max_pa_distance_mean_long",default=0,
+                        help="max pa_distance_mean_long",
+    )
+
+    parser.add_argument("-max-loss_sum_long","-max-loss_sum_long",
+                        type=float,required=False,dest="max_loss_sum_long",default=0,
+                        help="max_loss_sum_long",
+    )
+
     parser.add_argument("-bd-dir","--bd-dir",
                         type=str,required=False,dest="bd_dir",default="",
                         help="Parse all this directory to find backtest",
@@ -170,8 +185,8 @@ for file in files:
     n_days              = bt['result']['n_days']
     hrs_stuck_avg_long  = bt['result']['hrs_stuck_avg_long']
     hrs_stuck_max_long  = bt['result']['hrs_stuck_max_long']
-    hrs_stuck_avg_short  = bt['result']['hrs_stuck_avg_short']
-    hrs_stuck_max_short  = bt['result']['hrs_stuck_max_short']
+    # hrs_stuck_avg_short  = bt['result']['hrs_stuck_avg_short']
+    # hrs_stuck_max_short  = bt['result']['hrs_stuck_max_short']
     # n_entries_long      = bt['result']['n_entries_long']
     # n_unstuck_entries_long  = bt['result']['n_unstuck_entries_long']
     # n_unstuck_closes_long  = bt['result']['n_unstuck_closes_long']
@@ -179,22 +194,25 @@ for file in files:
     # starting_balance    = bt['result']['starting_balance']
 
     adg_perct           = bt['result']['adg_long']*100
-    adg_perct           += bt['result']['adg_short']*100
+    # adg_perct           += bt['result']['adg_short']*100
 
     
     gain_pct            = bt['result']['gain_long']*100 
-    gain_pct            += bt['result']['gain_short']*100 
+    # gain_pct            += bt['result']['gain_short']*100 
 
     gain_dollard        = bt['result']['final_balance_long'] -  bt['result']['starting_balance']
-    gain_dollard        += bt['result']['final_balance_short'] -  bt['result']['starting_balance']
+    # gain_dollard        += bt['result']['final_balance_short'] -  bt['result']['starting_balance']
 
     closest_bkr_long    = bt['result']['closest_bkr_long']
     closest_bkr_short    = bt['result']['closest_bkr_short']
 
+    eqbal_ratio_min_long    = bt['result']['eqbal_ratio_min_long']
+    pa_distance_mean_long    = bt['result']['pa_distance_mean_long']
+
     if (closest_bkr_long < args.min_closest_bkr) :
         continue
-    if (closest_bkr_short < args.min_closest_bkr) :
-        continue
+    # if (closest_bkr_short < args.min_closest_bkr) :
+    #     continue
     
     if (hrs_stuck_avg_long > args.max_stuck_avg) :
         continue
@@ -202,13 +220,29 @@ for file in files:
     if (hrs_stuck_max_long > args.max_stuck) :
         continue
     
-    if (hrs_stuck_avg_short > args.max_stuck_avg) :
+    # if (hrs_stuck_avg_short > args.max_stuck_avg) :
+    #     continue
+    
+    # if (hrs_stuck_max_short > args.max_stuck) :
+    #     continue
+    
+    if (eqbal_ratio_min_long < args.min_eqbal_ratio_min_long) :
         continue
     
-    if (hrs_stuck_max_short > args.max_stuck) :
-        continue
-    
+    loss_sum = bt['result']['loss_sum_long']
+
+    if abs(args.max_loss_sum_long) > 0:
+        if loss_sum == 0.0:
+            continue
+
+        if loss_sum < -abs(args.max_loss_sum_long):
+            continue
+
+
     if (gain_pct < args.min_gain) :
+        continue
+
+    if pa_distance_mean_long > args.max_pa_distance_mean_long: 
         continue
 
     parent_dir = os.path.realpath(file).replace(dir_to_read, '').strip("/").split("/")[0]
@@ -231,19 +265,23 @@ for file in files:
     datas['n_days']                 = n_days
     datas['h_stuck_avg_l']     = hrs_stuck_avg_long
     datas['h_stuck_max_l']     = hrs_stuck_max_long
-    datas['h_stuck_avg_s']     = hrs_stuck_avg_short
-    datas['h_stuck_max_s']     = hrs_stuck_max_short
+    # datas['h_stuck_avg_s']     = hrs_stuck_avg_short
+    # datas['h_stuck_max_s']     = hrs_stuck_max_short
 
+    datas['pa_distance_mean_long']     = pa_distance_mean_long
     datas['l_gridspan']     = l_grid_span
-    datas['s_gridspan']     = s_grid_span
+    # datas['s_gridspan']     = s_grid_span
+
+    datas['l_eqbal_ratio_min_long']     = eqbal_ratio_min_long
+    
 
     datas['profit_sum_long']     = bt['result']['profit_sum_long']
-    datas['loss_sum_long']     = bt['result']['loss_sum_long']
+    datas['loss_sum_long']     = loss_sum
     datas['l_ratio_loss_profit']     = bt['result']['loss_sum_long'] / bt['result']['profit_sum_long'] if bt['result']['profit_sum_long'] > 0 else 0
 
-    datas['profit_sum_short']     = bt['result']['profit_sum_short']
-    datas['loss_sum_short']     = bt['result']['loss_sum_short']
-    datas['s_ratio_loss_profit']     = bt['result']['loss_sum_short'] / bt['result']['profit_sum_short'] if bt['result']['profit_sum_short'] > 0 else 0
+    # datas['profit_sum_short']     = bt['result']['profit_sum_short']
+    # datas['loss_sum_short']     = bt['result']['loss_sum_short']
+    # datas['s_ratio_loss_profit']     = bt['result']['loss_sum_short'] / bt['result']['profit_sum_short'] if bt['result']['profit_sum_short'] > 0 else 0
 
 
 
@@ -253,13 +291,13 @@ for file in files:
     # datas['n_unstuck_closes_long']     = n_unstuck_closes_long
     # datas['loss_sum_long']     = loss_sum_long
     datas['l_adg %'] = bt['result']['adg_long']*100
-    datas['s_adg %'] = bt['result']['adg_short']*100
+    # datas['s_adg %'] = bt['result']['adg_short']*100
     datas['total adg %']                  = adg_perct
     datas['total gain %']                 = gain_pct
     datas['total gain $']                 = gain_dollard
     # datas['starting balance']       = starting_balance
     datas['bkr Long']            = closest_bkr_long
-    datas['bkr_Short']            = closest_bkr_short
+    # datas['bkr_Short']            = closest_bkr_short
     
     
 
