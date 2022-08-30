3fd05bacc57db698ead1ddd3db05d78a3dc1aa1e
tedyptedto <nomp5923@gmail.com>
2022-08-30 22:19:07 +0200

    update server

--- run_server_live_bybit_tedy.sh
+++ run_server_live_bybit_tedy.sh
@@ -7,6 +7,6 @@ for i in "${symbols[@]}"
 do
     :
     echo "Running screen on $i"
-    screen -S "bybit_tedy_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_tedy $i  configs/live/a_b4483_only_long_we_1.json "
+    screen -S "bybit_tedy_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_tedy $i  configs/live/a_57123_long_we_1.json "
 done
 
