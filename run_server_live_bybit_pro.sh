5fa41c43f0a562fedde0b151dab4cd53c768cb4f
tedyptedto <nomp5923@gmail.com>
2022-09-18 17:51:41 +0200

    update strat

--- run_server_live_bybit_pro.sh
+++ run_server_live_bybit_pro.sh
@@ -8,6 +8,7 @@ do
     :
     echo "Running screen on $i"
     # screen -S "bybit_pro_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_pro $i  configs/live/a_pro.json "
-    screen -S "bybit_pro_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_pro $i  configs/live/eb1ed_long_only_pro.json "
+    # screen -S "bybit_pro_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_pro $i  configs/live/eb1ed_long_only_pro.json "
+    screen -S "bybit_pro_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_pro $i  configs/live/a_57123_long_we_1.json "
 done
 
