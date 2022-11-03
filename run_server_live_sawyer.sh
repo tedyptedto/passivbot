8cf2aa379b2038dafe664169d2bb5eb43233c5a3
tedyptedto <nomp5923@gmail.com>
2022-11-03 21:42:29 +0100

    update sawyer

--- run_server_live_sawyer.sh
+++ run_server_live_sawyer.sh
@@ -2,7 +2,7 @@
 current_pwd=`pwd`
 gs=' -gs '
 gs=''
-symbols=(CHZUSDT ALICEUSDT MATICUSDT CRVUSDT )
+symbols=(CHZUSDT MATICUSDT CRVUSDT )
 for i in "${symbols[@]}"
 do
     :
@@ -11,3 +11,14 @@ do
     sleep 1
 done
 
+# current_pwd=`pwd`
+gs=' -gs '
+# gs=''
+symbols=( ALICEUSDT   )
+for i in "${symbols[@]}"
+do
+    :
+    echo "Running screen on $i"
+    screen -S "sawyer_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_sawyer $i  configs/live/a_sawyer.json "
+    sleep 1
+done
