d1e7adcd73181af502519eecdaeab10de95d9dd2
tedyptedto <nomp5923@gmail.com>
2023-10-29 23:44:27 +0100

    main scripts

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,3 @@
 source ./run_server_live_bybit_tedy.sh
+source ./run_server_live_bybit_tedyptedtoSUB1.sh
 source ./run_server_live_sawyer.sh
--- stop_server.sh
+++ stop_server.sh
@@ -1,2 +1,3 @@
 source ./stop_server_live_bybit_tedy.sh
+source ./stop_server_live_bybit_tedyptedtoSUB1.sh
 source ./stop_server_live_sawyer.sh
