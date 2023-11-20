344266092b4ea09fdad8f5002c9ab9182ed31c32
tedyptedto <nomp5923@gmail.com>
2023-11-20 21:37:20 +0100

    new strategy

--- run_server_live_bybit_tedyptedtoSUB2.sh
+++ run_server_live_bybit_tedyptedtoSUB2.sh
@@ -7,6 +7,6 @@ for i in "${symbols[@]}"
 do
     :
     echo "Running screen on $i"
-    screen -S "bybit_tedySUB1_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_tedySUB2 $i  configs/live/a_tedy_be550.json "
+    screen -S "bybit_tedySUB2_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_tedySUB2 $i  configs/live/a_tedy_be550.json "
 done
 
