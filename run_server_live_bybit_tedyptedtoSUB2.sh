5999670cdb4cc0fb98e2ae18a319715a49d0a3c3
tedyptedto <nomp5923@gmail.com>
2023-11-20 21:34:58 +0100

    new strategy

---
+++ run_server_live_bybit_tedyptedtoSUB2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+current_pwd=`pwd`
+gs=' -gs '
+gs=''
+symbols=(ADAUSDT SOLUSDT DOGEUSDT )
+for i in "${symbols[@]}"
+do
+    :
+    echo "Running screen on $i"
+    screen -S "bybit_tedySUB1_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_tedySUB2 $i  configs/live/a_tedy_be550.json "
+done
+
