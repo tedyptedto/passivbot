894bf63723f9d0bd72c287125d1798a6f8d643d9
tedyptedto <nomp5923@gmail.com>
2024-03-01 23:31:27 +0100

    testing multi

--- run_server_live_bybit_pro.sh
+++ run_server_live_bybit_pro.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 current_pwd=`pwd`
-gs=' -gs '
-gs=''
-symbols=(MATICUSDT DOGEUSDT AVAXUSDT SOLUSDT DOTUSDT UNIUSDT XRPUSDT ADAUSDT TRXUSDT LTCUSDT )
-for i in "${symbols[@]}"
-do
-    :
-    echo "Running screen on $i"
-    screen -S "bybit_pro_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_pro $i  configs/live/_running/pro/a_57123_long_we_pro.json "
-done
+symbols="MATICUSDT,DOGEUSDT,AVAXUSD,SOLUSDT,DOTUSDT,UNIUSDT,XRPUSDT,ADAUSDT,TRXUSDT,LTCUSDT"
+config="configs/live/_running/pro/a_57123_long_we_pro.json"
+i="bybit_pro_multi"
+twe_long="2"
+bbuser="bybit_pro"
 
+echo "Running screen $i"
+screen -S "$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u ${bbuser} -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
