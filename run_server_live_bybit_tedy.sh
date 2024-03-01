996094b4d2de5dc777f8b902fd29e619ab6b1e22
tedyptedto <nomp5923@gmail.com>
2024-03-01 22:20:34 +0100

    testing multi

--- run_server_live_bybit_tedy.sh
+++ run_server_live_bybit_tedy.sh
@@ -3,6 +3,7 @@ current_pwd=`pwd`
 symbols="XRPUSDT,MATICUSDT,DOGEUSDT"
 config="configs/live/_running/tedy/a_57123_long_we_1.json"
 i="bybit_tedy_multi"
+twe_long="3"
 
 echo "Running screen $i"
-screen -S "$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot_multi.py -u bybit_tedy -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
+screen -S "$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u bybit_tedy -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
--- run_server_live_bybit_tedyptedtoSUB2.sh
+++ run_server_live_bybit_tedyptedtoSUB2.sh
@@ -3,6 +3,7 @@ current_pwd=`pwd`
 symbols="ADAUSDT,SOLUSDT,DOGEUSDT"
 config="configs/live/_running/tedySUB2/a_tedy_be550.json"
 i="bybit_tedySUB2_multi"
+twe_long="2"
 
 echo "Running screen $i"
-screen -S "$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot_multi.py -u bybit_tedy -s ${symbols} -dcp ${config} configs/live/_running/config.hjson"
+screen -S "$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u bybit_tedy -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
