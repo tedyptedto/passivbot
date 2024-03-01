f3c26dbaf15893a6e23db2fa4f0c41dbd93c06e1
tedyptedto <nomp5923@gmail.com>
2024-03-01 22:32:52 +0100

    testing multi

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,9 @@
 source ./run_server_live_bybit_tedy.sh
+sleep 4
 source ./run_server_live_bybit_tedyptedtoSUB1.sh
+sleep 4
 source ./run_server_live_bybit_tedyptedtoSUB2.sh
+sleep 4
 source ./run_server_live_bybit_tedyptedtoSUB3.sh
+sleep 4
 source ./run_server_live_sawyer.sh
