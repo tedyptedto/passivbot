1dd2731f034804f5be042f92ab107b19bd986555
tedyptedto <nomp5923@gmail.com>
2024-03-06 22:04:59 +0100

    backtest attempt

--- run_backtest_pro.sh
+++ run_backtest_pro.sh
@@ -6,5 +6,7 @@ i="bybit_pro_multi"
 twe_long="2"
 bbuser="bybit_pro"
 
-echo "Running screen $i"
-screen -S "$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u ${bbuser} -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
+cd ${current_pwd}/;
+# not allowed : -dcp ${config} <= must change in multi.hjson
+
+python3 backtest_multi.py -le y -se n -tl ${twe_long} -u binance_01 -s ${symbols}  -bc configs/backtest/_running/multi.hjson 
