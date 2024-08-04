6f19a9149b97fc3efc8f9980db0181efe867db21
tedyptedto <nomp5923@gmail.com>
2024-08-04 13:12:00 +0200

    vault

--- run_tedy/tedy_run.sh
+++ run_tedy/tedy_run.sh
@@ -12,7 +12,7 @@ symbols="XRPUSDT,MATICUSDT,DOGEUSDT"
 config="configs/live/_running/tedy/a_57123_long_we_1.json"
 i="HL_tedy_57123"
 twe_long="2.5"
-bbuser="hyperliquid_main"
+bbuser="hyperliquid_vault_tedy57123"
 
 echo "Running screen $i"
 screen -S "$i" -dm bash -c "cd ${current_pwd};python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u ${bbuser} -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
