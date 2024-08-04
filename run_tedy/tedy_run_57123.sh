1cef9a9dc551d659d7c824fd5e858a0048448374
tedyptedto <nomp5923@gmail.com>
2024-08-04 22:08:48 +0200

    update scripts

--- run_tedy/tedy_run_57123.sh
+++ run_tedy/tedy_run_57123.sh
@@ -18,13 +18,3 @@ echo "Running screen $i"
 screen -S "$i" -dm bash -c "cd ${current_pwd};python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u ${bbuser} -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
 
 
-#   Tedy2
-# current_pwd=`pwd`
-# symbols="ADAUSDT,SOLUSDT,DOGEUSDT"
-# config="configs/live/_running/tedySUB2/a_tedy_be550.json"
-# i="HL_tedy_be550"
-# twe_long="3"
-# bbuser="bybit_tedySUB2"
-
-# echo "Running screen $i"
-# screen -S "$i" -dm bash -c "cd ${current_pwd};python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u ${bbuser} -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
---
+++ run_tedy/tedy_run_be550.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/Documents/passivbot5.4.2tedy
+
+# Get script from 'old' and after 
+# Changed "i" to better name
+# Changed "bbuser" to use HL account
+
+
+#   Tedy2
+current_pwd=`pwd`
+symbols="ADAUSDT,SOLUSDT,DOGEUSDT"
+config="configs/live/_running/tedySUB2/a_tedy_be550.json"
+i="HL_tedy_be550"
+twe_long="3"
+bbuser="hyperliquid_vault_tedybe550"
+
+echo "Running screen $i"
+screen -S "$i" -dm bash -c "cd ${current_pwd};python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u ${bbuser} -s ${symbols} -dcp ${config} configs/live/_running/config.hjson "
