0d974cf2c1c66748d40cb5bde2328ecd2e71c4f7
tedyptedto <99255539+tedyptedto@users.noreply.github.com>
2024-08-09 16:14:14 +0200

    Update scripts

---
+++ run_tedy/pro_run_57123.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ~/passivbot5.4.2pro
+
+# Get script from 'old' and after 
+# Changed "i" to better name
+# Changed "bbuser" to use HL account
+
+#   Tedy1
+current_pwd=`pwd`
+symbols="MATICUSDT,DOGEUSDT,AVAXUSDT,SOLUSDT,DOTUSDT,UNIUSDT,XRPUSDT,ADAUSDT,TRXUSDT,LTCUSDT"
+config="configs/live/_running/pro/a_57123_long_we_pro.json"
+i="HL_pro_57123"
+twe_long="2"
+bbuser="hyperliquid_pro57123"
+
+echo "Running screen $i"
+screen -S "$i" -dm bash -c "cd ${current_pwd}; while true; do python3 passivbot_multi.py -le y -se n -tl ${twe_long} -u ${bbuser} -s ${symbols} -dcp ${config} configs/live/_running/config.hjson; echo 'Waiting 2.5 minutes before restart...'; sleep 150; done"
+
+
---
+++ run_tedy/pro_stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+i="HL_pro_57123"
+echo "Kill screen for $i"
+screen -S "$i" -X quit
+
