b1d625fcb6ae546d6528f82ce64731e02e3b44ef
tedyptedto <nomp5923@gmail.com>
2024-08-15 16:54:18 +0200

    new print

--- scripts/discord/actions/hyperliquid.py
+++ scripts/discord/actions/hyperliquid.py
@@ -97,9 +97,12 @@ async def allHL(message, isAuto):
 
         user_name = user_name.replace('hyperliquid_', '')
 
+        await message.channel.send("v---**" + user_name + "**---v")
+
         #                                           ### Build message / Discord mobile = 29 caractères
         messageToSend = ""
-        messageToSend += f"{user_name.upper():<15} {usdc_value:>12,.2f}$\n"
+        # messageToSend += f"{user_name.upper():<15} {usdc_value:>12,.2f}$\n"
+        messageToSend += f"{' ':<15} {usdc_value:>12,.2f}$\n"
         messageToSend += f"..............................\n"
         if nbFollowers > 0:
             messageToSend += f"{'NbFollowers':<16} {'Equ. Foll.':>12}\n"
